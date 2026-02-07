f11cc05206434af7c35f9c3fcfdf689e6ef3b45a
Abhishek Singh <96238644+Abhishek-Singh5@users.noreply.github.com>
2026-02-07 10:55:58 +0530

    Implement method overloading for sum function

---
+++ Simple_Override.java
@@ -0,0 +1,20 @@
+class Main {
+    
+    public static int sum(int a, int b){
+        return a + b;
+    }
+
+    
+    public static double sum(double a, double b, double c){
+        return a + b + c;
+    }
+    
+
+
+    public static void main(String []q){
+        
+        System.out.println(sum(7, 4));
+        System.out.println(sum(6.3 , 4.8, 8.99));
+        
+    }
+}
